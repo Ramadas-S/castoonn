8057ed3a190f21c3cbd068d92c5696da5c27a868
Ramadas-S <ramadass033@gmail.com>
2023-07-05 16:54:32 +0530

    2nd commit

--- CastoonnApp/urls.py
+++ CastoonnApp/urls.py
@@ -4,8 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index, name='index'),
+     ################################ register detail
+    path('reg_details/', views.reg_details, name='reg_details'),
+    
 
     ################################ register
     path('register/',views.register,name='register'),
-    
+   
+
     ]
--- CastoonnApp/views.py
+++ CastoonnApp/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'index/index.html')
 
+def reg_details(request):
+
+    return render(request,'index/reg_details.html')
+
 
 def register(request):
 
